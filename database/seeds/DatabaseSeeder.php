47bc40a24d587e62ed9286fb27daf88d738ced28
masakisaaan <m.hamada@pixelgram.jp>
2017-10-02 06:49:04 +0900

    Add Sample Userdata

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        $this->call('UsersSeeder');
+
+        Model::reguard();
     }
 }
+
+    class UsersSeeder extends Seeder
+    {
+        public function run()
+        {
+            DB::table('users')->delete();
+
+            //test data
+            User::create([
+                'name' => '濱田　真旗',
+                'kana' =>  'テスト　タロウ',
+                'email' => 'b5163@oic.jp',
+                'birthday' => 19960507,
+                'access_token' => 'himitu',
+                'refresh_token' => 'naisyo',
+
+                ]);
+        }
+    }
